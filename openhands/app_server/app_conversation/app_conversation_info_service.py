84ed3a4b2ce041ece8d0c32434702335dc9e092b
hieptl <hieptl.developer@gmail.com>
2025-12-02 17:28:46 +0700

    refactor: remove update_conversation_statistics from AppConversationInfoService

--- openhands/app_server/app_conversation/app_conversation_info_service.py
+++ openhands/app_server/app_conversation/app_conversation_info_service.py
@@ -9,7 +9,6 @@ from openhands.app_server.app_conversation.app_conversation_models import (
     AppConversationSortOrder,
 )
 from openhands.app_server.services.injector import Injector
-from openhands.sdk.conversation.conversation_stats import ConversationStats
 from openhands.sdk.event import ConversationStateUpdateEvent
 from openhands.sdk.utils.models import DiscriminatedUnionMixin
 
@@ -92,17 +91,6 @@ class AppConversationInfoService(ABC):
         """Store the sandboxed conversation info object given.
 
         Return the stored info
-        """
-
-    @abstractmethod
-    async def update_conversation_statistics(
-        self, conversation_id: UUID, stats: ConversationStats
-    ) -> None:
-        """Update conversation statistics from stats event data.
-
-        Args:
-            conversation_id: The ID of the conversation to update
-            stats: ConversationStats object containing usage_to_metrics data from stats event
         """
 
     @abstractmethod
