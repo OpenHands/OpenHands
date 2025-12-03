a5c25bfbcd0f18ceea2bace796f10d0b3983ef5e
hieptl <hieptl.developer@gmail.com>
2025-12-03 21:07:00 +0700

    refactor: user-context-menu.tsx

--- frontend/src/components/features/user/user-context-menu.tsx
+++ frontend/src/components/features/user/user-context-menu.tsx
@@ -156,7 +156,7 @@ export function UserContextMenu({ type, onClose }: UserContextMenuProps) {
           />
         </div>
 
-        {isUser && (
+        {!isUser && (
           <>
             <TempButton
               onClick={handleInviteMemberClick}
