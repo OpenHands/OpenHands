aea2b1bbcd387fd95701c4bf82259e1c6ebecf2c
Tim O'Farrell <tofarr@gmail.com>
2025-12-16 16:21:42 -0700

    Fix for frontend stall

--- frontend/src/utils/parse-terminal-output.ts
+++ frontend/src/utils/parse-terminal-output.ts
@@ -1,3 +1,5 @@
+const START = "[Python Interpreter: ";
+
 /**
  * Parses the raw output from the terminal into the command and symbol
  * @param raw The raw output to be displayed in the terminal
@@ -13,9 +15,11 @@
  * console.log(parsed.symbol); // openhands@659478cb008c:/workspace $
  */
 export const parseTerminalOutput = (raw: string) => {
-  const envRegex = /(.*)\[Python Interpreter: (.*)\]/s;
-  const match = raw.match(envRegex);
-
-  if (!match) return raw;
-  return match[1]?.trim() || "";
+  let start = raw.indexOf("[Python Interpreter: ");
+  if (start < 0) {
+    return raw;
+  }
+  start += START.length;
+  const end = raw.indexOf("]", start);
+  return raw.substring(start, end).trim();
 };
