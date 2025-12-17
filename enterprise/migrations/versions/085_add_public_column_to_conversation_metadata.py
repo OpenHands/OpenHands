e724c1daed57a21f9c61ab07211944cff915d431
Tim O'Farrell <tofarr@gmail.com>
2025-12-17 13:48:48 -0700

    Fixed merge errors

--- enterprise/migrations/versions/085_add_public_column_to_conversation_metadata.py
+++ enterprise/migrations/versions/085_add_public_column_to_conversation_metadata.py
@@ -1,7 +1,7 @@
 """add public column to conversation_metadata
 
-Revision ID: 084
-Revises: 083
+Revision ID: 085
+Revises: 084
 Create Date: 2025-01-27 00:00:00.000000
 
 """
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '084'
-down_revision: Union[str, None] = '083'
+revision: str = '085'
+down_revision: Union[str, None] = '084'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
