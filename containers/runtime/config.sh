b3a60cdaa105d93d07fc6a941dd1f8da4c8a8c70
Brent G <deliberatekids@gmail.com>
2026-01-16 00:24:15 +0000

    Update container configuration for local development
    - Modified runtime config script with latest settings
    - Added local development compose configuration

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -5,3 +5,7 @@ DOCKER_IMAGE=runtime
 # These variables will be appended by the runtime_build.py script
 # DOCKER_IMAGE_TAG=
 # DOCKER_IMAGE_SOURCE_TAG=
+
+DOCKER_IMAGE_TAG=oh_v1.1.0_image_nikolaik_s_python-nodejs_tag_python3.12-nodejs22
+DOCKER_IMAGE_SOURCE_TAG=oh_v1.1.0_ndgknrzgymxnm11d_iru6h1om3sbzh7ks
+
