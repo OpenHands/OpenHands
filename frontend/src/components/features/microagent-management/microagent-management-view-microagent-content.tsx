a55dce75ef1b36fa453445a5b58b76bf6631645f
naaa760 <neh6a683@gmail.com>
2026-01-24 15:40:26 +0530

    Migrate microagent management and diff viewer to new Spinner

--- frontend/src/components/features/microagent-management/microagent-management-view-microagent-content.tsx
+++ frontend/src/components/features/microagent-management/microagent-management-view-microagent-content.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from "react-i18next";
-import { Spinner } from "@heroui/react";
 import { useMicroagentManagementStore } from "#/stores/microagent-management-store";
 import { useRepositoryMicroagentContent } from "#/hooks/query/use-repository-microagent-content";
 import { I18nKey } from "#/i18n/declaration";
 import { extractRepositoryInfo } from "#/utils/utils";
 import { MarkdownRenderer } from "../markdown/markdown-renderer";
+import { Spinner } from "#/components/shared/spinner";
 
 export function MicroagentManagementViewMicroagentContent() {
   const { t } = useTranslation();
