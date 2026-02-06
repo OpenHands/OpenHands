7a0ecdd46b8e3aabc943d6f16d557ff4d96c556d
tofarr <tofarr@gmail.com>
2026-02-06 10:59:34 +0000

    Lint fixes

--- enterprise/saas_server.py
+++ enterprise/saas_server.py
@@ -78,9 +78,11 @@ base_app.include_router(shared_event_router)
 
 # Add GitHub integration router only if GITHUB_APP_CLIENT_ID is set
 if GITHUB_APP_CLIENT_ID:
-    from server.routes.integration.github import github_integration_router  # noqa: E402
     # Make sure that the callback processor is loaded here so we don't get an error when deserializing
-    from integrations.github.github_v1_callback_processor import GithubV1CallbackProcessor  # noqa: E402
+    from integrations.github.github_v1_callback_processor import (  # noqa: E402
+        GithubV1CallbackProcessor,
+    )
+    from server.routes.integration.github import github_integration_router  # noqa: E402
 
     # Bludgeon mypy into not deleting my import
     logger.debug(f'Loaded {GithubV1CallbackProcessor.__name__}')
