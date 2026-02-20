0e839e0081de46b42bbfe175d068873398cdc860
tofarr <tofarr@gmail.com>
2026-02-20 14:18:06 +0000

    WIP

--- enterprise/server/middleware.py
+++ enterprise/server/middleware.py
@@ -151,7 +151,7 @@ class SetAuthCookieMiddleware:
         if request.method == 'OPTIONS':
             return False
         path = request.url.path
-        logger.info("_should_attach", extra={"path": path})
+        logger.info('_should_attach', extra={'path': path})
 
         ignore_paths = (
             '/api/options/config',
