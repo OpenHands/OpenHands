7698aad0c609a02efe12c7fd11f618516dcff0bf
tofarr <tofarr@gmail.com>
2026-02-27 11:31:29 +0000

    Lint fixes

--- enterprise/server/verified_models/verified_model_router.py
+++ enterprise/server/verified_models/verified_model_router.py
@@ -2,7 +2,7 @@
 
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Query, status
+from fastapi import APIRouter, Depends, HTTPException, Query, Request, status
 from server.email_validation import get_admin_user_id
 from server.verified_models.verified_model_models import (
     VerifiedModel,
