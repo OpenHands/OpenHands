b251de19265a3bdf680debc8b0bb64409d8c505a
tofarr <tofarr@gmail.com>
2026-04-27 16:28:13 -0600

    Removed backwards compatibility

--- openhands/storage/data_models/secrets.py
+++
@@ -1,9 +0,0 @@
-# Re-exports from new location for backward compatibility.
-# All models have been moved to openhands.app_server.secrets.secrets_models
-from __future__ import annotations
-
-from openhands.app_server.secrets.secrets_models import Secrets
-
-__all__ = [
-    'Secrets',
-]
