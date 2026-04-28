044dfc6f64eb189ac0c7b88cbb4bf9d38b132212
tofarr <tofarr@gmail.com>
2026-04-28 07:21:14 -0600

    Lint fixes

--- enterprise/integrations/github/data_collector.py
+++ enterprise/integrations/github/data_collector.py
@@ -19,11 +19,11 @@ from server.auth.constants import GITHUB_APP_CLIENT_ID, GITHUB_APP_PRIVATE_KEY
 from storage.openhands_pr import OpenhandsPR
 from storage.openhands_pr_store import OpenhandsPRStore
 
+from openhands.app_server.file_store import get_file_store
 from openhands.core.config import load_openhands_config
 from openhands.core.logger import openhands_logger as logger
 from openhands.integrations.github.github_service import GithubServiceImpl
 from openhands.integrations.service_types import ProviderType
-from openhands.app_server.file_store import get_file_store
 
 config = load_openhands_config()
 file_store = get_file_store(config.file_store, config.file_store_path)
